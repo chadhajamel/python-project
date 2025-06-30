69d26914c2e656341579a3052a15e8f2e1669fed
JAMEL, Chadha <chadha.jamel@capgemini.com>
2025-06-30 15:02:06 +0100

    feat:add hello_component

---
+++ component.py
@@ -0,0 +1,10 @@
+import streamlit.components.v1 as components
+
+def hello_component(name):
+    components.html(f""" 
+ <div style='font-size: 100px'
+ onclick='alert("clicked!")'
+ Hello {name}> </div>
+""")
+
+hello_component('world')
